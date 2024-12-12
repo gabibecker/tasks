c164d61ea69b032a459c4c0f30243fcd740631b4
gabibecker <g.becker@unochapeco.edu.br>
2024-12-12 17:40:44 -0300

    Componente Lista de Tarefas #02

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground, StyleSheet } from "react-native";
+import todayImage from '../../assets/imgs/today.jpg'
 
 export default class TaskList extends Component {
     render() {
         return (
-            <View>
-                <Text>TaskList</Text>
+            <View style={style.container}>
+                <ImageBackground source={todayImage}
+                style={style.background}>
+                </ImageBackground>
+                <View style={style.taskList}>
+                       <Text>TaskList</Text> 
+                    </View>
             </View>
         )
     }
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    background: {
+        flex: 3
+    },
+    taskList: {
+        flex: 7
+    }
+
+})
